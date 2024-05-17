0001b9c72afe877b37f774924d567479ac9e6a58
Filipp Demchenko <filipp5632@gmail.com>
2024-05-17 13:20:03 +0300

    Create additional error helper and email validation

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,8 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	app.errorLog.Println(err)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func isCorrectEmail(email string) bool {
+	return EmailRX.MatchString(email)
 }
