2ea9169c8a4082ed57de01fe11c9a3b489c85eac
Filipp Demchenko <filipp5632@gmail.com>
2024-06-16 17:19:07 +0300

    Test secureHeaders middleware

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/", nil)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	secureHeaders(handler).ServeHTTP(recorder, request)
+	response := recorder.Result()
+	assert.Equal(t, response.StatusCode, http.StatusOK)
+
+	assert.Equal(t, "no-store", response.Header.Get("Cache-Control"))
+	assert.Equal(t, "frame-ancestors 'none'", response.Header.Get("Content-Security-Policy"))
+	assert.Equal(t, "nosniff", response.Header.Get("X-Content-Type-Options"))
+	assert.Equal(t, "DENY", response.Header.Get("X-Frame-Options"))
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, string(body), "OK")
+}
