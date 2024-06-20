c4f7ffb0095e9a0b4768fbf18e85014cd109b08a
Filipp Demchenko <filipp5632@gmail.com>
2024-06-20 15:23:00 +0300

    Combine validator tests into one

--- internal/validator/validator_test.go
+++ internal/validator/validator_test.go
@@ -6,46 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidator_ValidEmails(t *testing.T) {
+const InvalidEmailErrorMessage = "invalid email address"
+
+func TestValidator(t *testing.T) {
 	testCases := []struct {
-		name  string
-		email string
+		name                 string
+		email                string
+		isValid              bool
+		expectedErrorMessage string
 	}{
-		{name: "Hyphen in address", email: "some-email@address.com"},
-		{name: "Dot in address", email: "some.email@address.com"},
-		{name: "Multi domain", email: "someemail@address.edu.ua"},
-		{name: "Hyphen in the end", email: "someemail-@address.ua"},
+		{name: "Hyphen in address", email: "some-email@address.com", isValid: true},
+		{name: "Dot in address", email: "some.email@address.com", isValid: true},
+		{name: "Multi domain", email: "someemail@address.edu.ua", isValid: true},
+		{name: "Hyphen in the end", email: "someemail-@address.ua", isValid: true},
+		{name: "No domain", email: "some-email@", isValid: false, expectedErrorMessage: InvalidEmailErrorMessage},
+		{name: "Hash symbol in email", email: "abc.def@mail#archive.com", isValid: false,
+			expectedErrorMessage: InvalidEmailErrorMessage},
+		{name: "Without at sign", email: "someemailaddress.edu.ua", isValid: false,
+			expectedErrorMessage: InvalidEmailErrorMessage},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			v := New()
-			v.Check(IsValidEmail(tc.email), "email", "invalid email address")
+			v.Check(IsValidEmail(tc.email), "email", InvalidEmailErrorMessage)
 
-			assert.True(t, v.IsValid())
-		})
-	}
-}
-
-func TestValidator_InvalidEmails(t *testing.T) {
-	testCases := []struct {
-		name  string
-		email string
-	}{
-		{name: "No domain", email: "some-email@"},
-		{name: "Hash symbol in email", email: "abc.def@mail#archive.com"},
-		{name: "Without at sign", email: "someemailaddress.edu.ua"},
-	}
-
-	message := "invalid email address"
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			v := New()
-			v.Check(IsValidEmail(tc.email), "email", message)
-
-			assert.False(t, v.IsValid())
-			assert.Equal(t, message, v.Errors["email"])
+			assert.Equal(t, tc.isValid, v.IsValid())
+			assert.Equal(t, tc.expectedErrorMessage, v.Errors["email"])
 		})
 	}
 }
