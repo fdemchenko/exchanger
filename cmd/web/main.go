c1bad0ee2e65c36e97cd334a447d3880b9d409a3
Filipp Demchenko <filipp5632@gmail.com>
2024-07-02 00:23:25 +0300

    Extract command line flags parsing into separate function

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,30 +39,7 @@ const (
 )
 
 func main() {
-	var cfg config
-	flag.StringVar(&cfg.addr, "addr", ":8080", "http listen address")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("EXCHANGER_DSN"), "Data source name")
-	flag.IntVar(&cfg.db.maxConnections, "db-max-conn", DefaultMaxDBConnections, "Database max connection")
-
-	flag.StringVar(&cfg.mailer.Host, "smtp-host", os.Getenv("EXCHANGER_SMPT_HOST"), "Smpt host")
-	flag.IntVar(&cfg.mailer.Port, "smtp-port", DefaultSMTPPort, "Smpt port")
-	flag.StringVar(&cfg.mailer.Username, "smtp-username", os.Getenv("EXCHANGER_SMPT_USERNAME"), "Smpt username")
-	flag.StringVar(&cfg.mailer.Password, "smtp-password", os.Getenv("EXCHANGER_SMPT_PASSWORD"), "Smpt password")
-	flag.StringVar(&cfg.mailer.Sender, "smtp-sender", os.Getenv("EXCHANGER_SMPT_SENDER"), "Smpt sender")
-	flag.Func("mailer-interval", "Email update interval (E.g. 24h, 1h30m)", func(s string) error {
-		if s == "" {
-			cfg.mailer.UpdateInterval = DefaultMailerInterval
-			return nil
-		}
-		duration, err := time.ParseDuration(s)
-		if err != nil {
-			return err
-		}
-		cfg.mailer.UpdateInterval = duration
-		return nil
-	})
-	flag.Parse()
-
+	cfg := initConfig()
 	zerolog.TimeFieldFormat = time.RFC3339
 
 	db, err := openDB(cfg)
@@ -101,6 +78,33 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
+}
+
+func initConfig() config {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":8080", "http listen address")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("EXCHANGER_DSN"), "Data source name")
+	flag.IntVar(&cfg.db.maxConnections, "db-max-conn", DefaultMaxDBConnections, "Database max connection")
+
+	flag.StringVar(&cfg.mailer.Host, "smtp-host", os.Getenv("EXCHANGER_SMPT_HOST"), "Smpt host")
+	flag.IntVar(&cfg.mailer.Port, "smtp-port", DefaultSMTPPort, "Smpt port")
+	flag.StringVar(&cfg.mailer.Username, "smtp-username", os.Getenv("EXCHANGER_SMPT_USERNAME"), "Smpt username")
+	flag.StringVar(&cfg.mailer.Password, "smtp-password", os.Getenv("EXCHANGER_SMPT_PASSWORD"), "Smpt password")
+	flag.StringVar(&cfg.mailer.Sender, "smtp-sender", os.Getenv("EXCHANGER_SMPT_SENDER"), "Smpt sender")
+	flag.Func("mailer-interval", "Email update interval (E.g. 24h, 1h30m)", func(s string) error {
+		if s == "" {
+			cfg.mailer.UpdateInterval = DefaultMailerInterval
+			return nil
+		}
+		duration, err := time.ParseDuration(s)
+		if err != nil {
+			return err
+		}
+		cfg.mailer.UpdateInterval = duration
+		return nil
+	})
+	flag.Parse()
+	return cfg
 }
 
 func openDB(cfg config) (*sql.DB, error) {
