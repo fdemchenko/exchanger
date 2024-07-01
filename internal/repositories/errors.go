efabb2170189b56c28496dd25b495b4b842bf538
Filipp Demchenko <filipp5632@gmail.com>
2024-07-01 16:56:10 +0300

    Extract PostgreSQL error code into constant

--- internal/repositories/errors.go
+++ internal/repositories/errors.go
@@ -2,4 +2,8 @@ package repositories
 
 import "errors"
 
-var ErrDuplicateEmail = errors.New("email already exists")
+var (
+	ErrDuplicateEmail = errors.New("email already exists")
+)
+
+const PostgreSQLUniqueViolationErrorCode = "23505"
