67346bbd240b41aabc33f9c7ceeb1a7798397864
Filipp Demchenko <filipp5632@gmail.com>
2024-07-05 21:24:24 +0300

    Add connection pool size parameter

--- cmd/mailer/config.go
+++ cmd/mailer/config.go
@@ -24,6 +24,11 @@ var ServiceConfig Config
 func init() {
 	flag.StringVar(&ServiceConfig.SMTP.Host, "smtp-host", os.Getenv("EXCHANGER_SMTP_HOST"), "Smtp host")
 	flag.IntVar(&ServiceConfig.SMTP.Port, "smtp-port", DefaultSMTPPort, "Smtp port")
+	flag.IntVar(&ServiceConfig.SMTP.ConnectionPoolSize,
+		"smtp-connections",
+		DefaultMailerConnectionPoolSize,
+		"Smtp connection pool size",
+	)
 	flag.StringVar(&ServiceConfig.SMTP.Username, "smtp-username", os.Getenv("EXCHANGER_SMTP_USERNAME"), "Smtp username")
 	flag.StringVar(&ServiceConfig.SMTP.Password, "smtp-password", os.Getenv("EXCHANGER_SMTP_PASSWORD"), "Smtp password")
 	flag.StringVar(&ServiceConfig.SMTP.Sender, "smtp-sender", os.Getenv("EXCHANGER_SMTP_SENDER"), "Smtp sender")
