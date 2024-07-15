ce377a494fe28801d443f4f34fcd98dff152b3bd
Filipp Demchenko <filipp5632@gmail.com>
2024-07-15 20:35:13 +0300

    Extract database setup logic into separate package

--- internal/database/migrations.go
+++ internal/database/migrations.go
@@ -2,28 +2,25 @@ package database
 
 import (
 	"database/sql"
-	"embed"
 	"errors"
+	"io/fs"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	"github.com/golang-migrate/migrate/v4/source/iofs" // source of migrations
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-//go:embed migrations/*sql
-var migrationsFS embed.FS
-
-// closeDB is used primarily for tests, where leaking connection causing error of performing snapshot.
-func AutoMigrate(db *sql.DB, closeDB bool) error {
+func AutoMigrate(db *sql.DB, migrationsFS fs.FS, migratinsPath string, dbName string, closeConn bool) error {
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		return err
 	}
-	fs, err := iofs.New(migrationsFS, "migrations")
+	source, err := iofs.New(migrationsFS, "migrations")
 	if err != nil {
 		return err
 	}
-	m, err := migrate.NewWithInstance("iofs", fs, "exchanger", driver)
+
+	m, err := migrate.NewWithInstance("iofs", source, dbName, driver)
 	if err != nil {
 		return err
 	}
@@ -31,14 +28,12 @@ func AutoMigrate(db *sql.DB, closeDB bool) error {
 	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		return err
 	}
-	if closeDB {
+	if closeConn {
 		serr, derr := m.Close()
 		if serr != nil {
 			return serr
 		}
-		if derr != nil {
-			return derr
-		}
+		return derr
 	}
 	return nil
 }
