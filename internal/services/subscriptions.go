527e034a45159a2b15d5d7f8f8e66dd05cd9ea8c
Filipp Demchenko <filipp5632@gmail.com>
2024-07-16 19:53:23 +0300

    Change naming email -> subscriptions

---
+++ internal/services/subscriptions.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"strings"
+)
+
+type SubscriptonsRepository interface {
+	Insert(email string) error
+	GetAll() ([]string, error)
+	DeleteByEmail(email string) error
+	DeleteByID(id int) error
+}
+
+type subscriptionServiceImpl struct {
+	subscriptionsRepository SubscriptonsRepository
+}
+
+func NewEmailService(subscriptionRepository SubscriptonsRepository) *subscriptionServiceImpl {
+	return &subscriptionServiceImpl{subscriptionsRepository: subscriptionRepository}
+}
+
+func (ss *subscriptionServiceImpl) Create(email string) error {
+	// email is case insensitive
+	email = strings.ToLower(email)
+	return ss.subscriptionsRepository.Insert(email)
+}
+
+func (ss *subscriptionServiceImpl) GetAll() ([]string, error) {
+	return ss.subscriptionsRepository.GetAll()
+}
+
+func (ss *subscriptionServiceImpl) DeleteByEmail(email string) error {
+	// email is case insensitive
+	email = strings.ToLower(email)
+	return ss.subscriptionsRepository.DeleteByEmail(email)
+}
+
+func (ss *subscriptionServiceImpl) DeleteByID(id int) error {
+	return ss.subscriptionsRepository.DeleteByID(id)
+}
---
+++ internal/services/subscriptions_test.go
@@ -0,0 +1,84 @@
+package services
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/fdemchenko/exchanger/internal/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+type SubscriptonsRepositoryMock struct {
+	emails []string
+}
+
+func (er *SubscriptonsRepositoryMock) GetAll() ([]string, error) {
+	return er.emails, nil
+}
+
+func (er *SubscriptonsRepositoryMock) Insert(email string) error {
+	if slices.Contains(er.emails, email) {
+		return repositories.ErrDuplicateEmail
+	}
+	er.emails = append(er.emails, email)
+	return nil
+}
+
+func (er *SubscriptonsRepositoryMock) DeleteByEmail(email string) error {
+	return nil
+}
+
+func (er *SubscriptonsRepositoryMock) DeleteByID(id int) error {
+	return nil
+}
+func TestEmailService_CreateEmails(t *testing.T) {
+	emailRepo := new(SubscriptonsRepositoryMock)
+	emails := []string{"example@mail.com", "school@edu.ua"}
+
+	emailService := NewEmailService(emailRepo)
+	for _, newEmail := range emails {
+		err := emailService.Create(newEmail)
+		assert.NoError(t, err)
+	}
+}
+
+func TestEmailService_CaseInsensitiveness(t *testing.T) {
+	emailRepo := new(SubscriptonsRepositoryMock)
+	emails := []string{"example@mail.com", "EXamPlE@maIl.Com"}
+
+	emailService := NewEmailService(emailRepo)
+	err := emailService.Create(emails[0])
+	assert.NoError(t, err)
+
+	err = emailService.Create(emails[1])
+	assert.ErrorIs(t, err, repositories.ErrDuplicateEmail)
+}
+
+func TestEmailService_GetEmails(t *testing.T) {
+	emailRepo := new(SubscriptonsRepositoryMock)
+	emails := []string{"example@mail.com", "school@edu.ua"}
+
+	emailService := NewEmailService(emailRepo)
+	for _, newEmail := range emails {
+		err := emailService.Create(newEmail)
+		assert.NoError(t, err)
+	}
+
+	emailsReturned, err := emailService.GetAll()
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, emails, emailsReturned)
+}
+
+func TestEmailService_CreateDuplicateEmail(t *testing.T) {
+	emailRepo := new(SubscriptonsRepositoryMock)
+	emails := []string{"example@mail.com", "school@edu.ua"}
+
+	emailService := NewEmailService(emailRepo)
+	for _, newEmail := range emails {
+		err := emailService.Create(newEmail)
+		assert.NoError(t, err)
+	}
+
+	err := emailService.Create(emails[0])
+	assert.Equal(t, err, repositories.ErrDuplicateEmail)
+}
